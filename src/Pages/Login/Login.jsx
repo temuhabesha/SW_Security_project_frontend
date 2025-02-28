acd9a6109352527636466fc0031c370a1b145d29
tomhabesha <temesgenwondim@gmail.com>
2025-03-01 00:21:05 +0300

    the commit of this project

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Login.css'
+import axios from 'axios'
 
 function Login() {
+  const[email,setemail] = useState('')
+  const[password,setpassword] = useState('')
+  console.log(email)
+  console.log(password);
+
+  async function handlesubmit(e) {
+    e.preventDefault();
+  try {
+      const responce = await axios.post("http://localhost:3000/login", {
+        email,
+        password,
+      });
+
+      alert(responce.data.msg);
+  } catch (error) {
+    alert(error.response.data.msg)
+  }
+  }
+
   return (
-    <div>
-      this is the login component
+    <div className="login">
+      <h1>LOGIN PAGE</h1>
+      <form onSubmit={handlesubmit}>
+        <div className="email">
+          <label htmlFor="email">email</label>
+          <input type="email" placeholder="enter email" onChange={e=>setemail(e.target.value)}/>
+        </div>
+        <div className="password">
+          <label htmlFor="password">password</label>
+          <input type="password" placeholder="password" onChange={e=>setpassword(e.target.value)}/>
+        </div>
+        <button type='submit'>LogIn</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Login
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,30 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import './Register.css'
+import { use } from 'react'
 
 function Register() {
+
+    const [username,setusername] = useState('')
+    const [firstname,setfirstname] = useState('')
+    const [lastname,setlastname] = useState('')
+    const [email,setemail] = useState('')
+    const [password,setpassword] = useState('')
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+       try {
+         const responce = await axios.post("http://localhost:3000/register", {
+           username,
+           firstname,
+           lastname,
+           email,
+           password,
+
+         });
+           alert(responce.data.msg)
+       } catch (error) {
+        alert(error.response.data.msg)
+       }
+    }
   return (
     <div className='register'>
       <h1>Sign Up</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="uname">
             <label htmlFor="username">username:-</label>
-            <input type="text" placeholder='username'/>
+            <input type="text" placeholder='username' onChange={e=>setusername(e.target.value)}/>
         </div>
         <div className="fname">
             <label htmlFor="firstname">firstname:-</label>
-            <input type="text" placeholder='firstname'/>
+            <input type="text" placeholder='firstname' onChange={e=>setfirstname(e.target.value)}/>
         </div>
         <div className="lname">
             <label htmlFor="lastname">lastname:-</label>
-            <input type="text" placeholder='lastname'/>
+            <input type="text" placeholder='lastname' onChange={e=>setlastname(e.target.value)}/>
         </div>
         <div className="uemail">
             <label htmlFor="email">email:-</label>
-            <input type="email" placeholder='email'/>
+            <input type="email" placeholder='email' onChange={e=>setemail(e.target.value)}/>
         </div>
         <div className="upassword">
             <label htmlFor="password">password:-</label>
-            <input type="text" placeholder='password'/>
+            <input type="text" placeholder='password' onChange={e=>setpassword(e.target.value)}/>
         </div>
         <button type='submit'>SignUp</button>
       </form>
